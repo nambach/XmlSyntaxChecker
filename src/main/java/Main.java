95c711e72790a0ec1a7d22e32a6a08c4560e9cdc
NAMBM <nambach2@Gmail.com>
2018-10-27 12:45:24 +0700

    Implement comparing mode [NamBM]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,50 @@
-import parser.EntitySyntaxChecker;
-import parser.XmlSyntaxChecker;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Entities;
+import org.jsoup.safety.Whitelist;
+import org.jsoup.select.Elements;
+import utils.FileUtils;
+import utils.TextUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Main {
-    public static void main(String[] args) {
-        XmlSyntaxChecker checker = new XmlSyntaxChecker();
-        String src = "<h1 checked aa=aa a=\"1\"b='\"2\"&'c=3 kl>     <img a=1 a=/>kjkk  <p l p>YEAH</h1>";
-//        String src = "<table><li></h3><li></h3>";
-        src = checker.check(src);
-        System.out.println(src);
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
 
-        EntitySyntaxChecker entitySyntaxChecker = new EntitySyntaxChecker();
-        src = entitySyntaxChecker.check(src);
+        String urlString = "https://hangchinhhieu.vn/collections/laptop";
+        test2(urlString);
 
-        System.out.println(src);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+
+    private static void test1(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        URLConnection connection = url.openConnection();
+        String src = FileUtils.getString(connection.getInputStream());
+
+        src = TextUtils.refineHtml(src);
+
+        FileUtils.exportFile(src, FileUtils.getFilePath("file-checker.xml"));
+    }
+
+    private static void test2(String urlString) throws IOException {
+        Document doc = Jsoup.connect(urlString).get();
+
+        Elements body = doc.select("body");
+        String src = body.outerHtml();
+
+        Document.OutputSettings settings = new Document.OutputSettings();
+        settings.escapeMode(Entities.EscapeMode.xhtml);
+
+        src = Jsoup.clean(src, "", Whitelist.relaxed(), settings);
+
+        FileUtils.exportFile(src, FileUtils.getFilePath("file-jsoup.xml"));
     }
 }
+
+//        String src = "<h1 checked aa=aa a=\"1\"b='\"2\"&'c=3 kl>     <img a=1 a=/>kjkk  <p l p>YEAH</h1>";
+//        String src = "<table><li></h3><li></h3>";
---
+++ src/main/java/utils/TextUtils.java
@@ -0,0 +1,66 @@
+package utils;
+
+import parser.EntitySyntaxChecker;
+import parser.XmlSyntaxChecker;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TextUtils {
+
+    public static String refineHtml(String src) {
+//        src = getBody(src);
+        src = removeMiscellaneousTags(src);
+
+        XmlSyntaxChecker xmlSyntaxChecker = new XmlSyntaxChecker();
+        EntitySyntaxChecker entitySyntaxChecker = new EntitySyntaxChecker();
+        src = xmlSyntaxChecker.check(src);
+        src = entitySyntaxChecker.check(src);
+
+        //crop one more time
+        src = getBody(src);
+        return src;
+    }
+
+    private static String getBody(String src) {
+        String result = src;
+
+        String expression = "<body.*?<[ ]*/[ ]*body[ ]*>";
+        Pattern pattern = Pattern.compile(expression);
+
+        Matcher matcher = pattern.matcher(result);
+
+        if (matcher.find()) {
+            result = matcher.group(0);
+        }
+
+        return result;
+    }
+
+    public static String removeMiscellaneousTags(String src) {
+        String result = src;
+        List<String> doubleTags = Arrays.asList("script");
+
+        for (String doubleTag : doubleTags) {
+            //Match <singleTag> that contains anything but the phrase "/>", with reluctant matches
+            String expression = "<tag.*?</tag[ ]*>".replaceAll("tag", doubleTag);
+            result = result.replaceAll(expression, "");
+        }
+
+
+        //Remove all comments
+        String expression = "<!--.*?-->";
+        result = result.replaceAll(expression, "");
+
+        //Remove all whitespace
+        expression = "&nbsp;?";
+        result = result.replaceAll(expression, "");
+
+        return result;
+    }
+}
+
+//        String tagRegex = "<\\w+\\s+[^>]*?(?:>|/>)";
+//        String attributeRegex = "(\\w+=\"[^/><]*?\")(\\w+=\"[^/><]*?\")";

2901819f1cea79d07cdaf312a5742d09c9540c1f
NAMBM <nambach2@Gmail.com>
2018-12-18 15:30:04 +0700

    Refactor code [NamBM]

--- src/main/java/TestChecker.java
+++ src/main/java/TestChecker.java
@@ -13,24 +13,4 @@ public class TestChecker {
         String src = FileUtils.readTextContent("src/main/resources/static/xml/book.xml");
         System.out.println(checker.check(src));
     }
-
-    private static void checkNormal() {
-        XmlSyntaxChecker checker = new XmlSyntaxChecker();
-
-        Map<String, String> map = new LinkedHashMap<>();
-        map.put("Attribute không value", "<h1 checked>YEAH</h1>");
-        map.put("Value không bọc trong cặp nháy", "<h1 aa=  aa><img a=a />YEAH</h1>");
-        map.put("Attribute dính liền nhau", "<h1 a=\"1\"b='2'c=3>YEAH</h1>");
-        map.put("Empty element", "<h1><img src=\"\"><br><hr/></h1>");
-        map.put("Lỗi đóng mở thẻ", "<li><a>Sach Moi</a></h3>");
-
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            System.out.println(entry.getKey());
-
-            System.out.println(entry.getValue());
-            System.out.println(checker.check(entry.getValue()));
-
-            System.out.println();
-        }
-    }
 }
--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -1,12 +1,10 @@
 package utils;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 public class FileUtils {
 
     public static String EMPTY_CONTENT = "";
-    public static String LOCATION = "src/main/resources/";
 
     public static String readTextContent(String filePath) {
         StringBuilder result = new StringBuilder(EMPTY_CONTENT);
@@ -24,38 +22,5 @@ public class FileUtils {
         } finally {
             return result.toString();
         }
-    }
-
-    public static String getFilePath(String fileName) {
-        return LOCATION + fileName;
-    }
-
-    public static boolean exportFile(String src, String filePath) {
-        try {
-            FileWriter fileWriter = new FileWriter(filePath);
-            BufferedWriter writer = new BufferedWriter(fileWriter);
-            writer.write(src);
-            writer.close();
-            fileWriter.close();
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public static String getString(InputStream stream) {
-
-        StringBuilder stringBuilder = new StringBuilder();
-        String line = null;
-
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-            while ((line = bufferedReader.readLine()) != null) {
-                stringBuilder.append(line);
-            }
-        } catch (IOException ignored) {
-        }
-
-        return stringBuilder.toString();
     }
 }
